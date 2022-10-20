226944e6975b34ce815926c0a32a0d5f1757a637
Frithjof Thorvik <frithjot@stud.ntnu.no>
2022-10-20 14:27:51 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./styles/misc/App.scss";
+
+const App: React.FC = (): JSX.Element => {
+  return <div className="app">Hello</div>;
+};
+
+export default App;
---
+++ src/index.tsx
@@ -0,0 +1,15 @@
+import App from "./App";
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import "./styles/misc/Index.scss";
+
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
