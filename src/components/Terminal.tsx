042897ab700b67ae1ee59cf6aa8a13f27c99626f
smfranci <smfranci@stud.ntnu.no>
2022-10-26 13:43:21 +0200

    Terminal now works with history and input field

--- src/components/Terminal.tsx
+++ src/components/Terminal.tsx
@@ -2,12 +2,41 @@ import React from "react";
 
 import "./Terminal.scss";
 
-interface ITerminalProps {}
+interface ITerminalProps {
+  handleKeyDown: (event: React.KeyboardEvent<HTMLInputElement>) => any;
+  handleChange: (event: React.ChangeEvent<HTMLInputElement>) => any;
+  value: string;
+  history: string[];
+  bottomRef: React.RefObject<HTMLDivElement> | null;
+}
 
-const Terminal: React.FC<ITerminalProps> = (): JSX.Element => {
+const Terminal: React.FC<ITerminalProps> = ({
+  handleKeyDown,
+  handleChange,
+  value,
+  history,
+  bottomRef,
+}): JSX.Element => {
   return (
     <div className="terminalContainer">
-      <div className="content">Hello</div>
+      <div className="terminalContent">
+        <div className="terminalHistory">
+          {history.map((text, index) => (
+            <p key={index}>{text}</p>
+          ))}
+          <div ref={bottomRef} />
+        </div>
+        <div className="terminalInputArea">
+          <span>{">"}</span>
+          <input
+            className="terminalInput"
+            type="text"
+            value={value}
+            onChange={handleChange}
+            onKeyDown={handleKeyDown}
+          />
+        </div>
+      </div>
     </div>
   );
 };
--- src/controllers/components/TerminalController.tsx
+++ src/controllers/components/TerminalController.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 import Terminal from "../../components/Terminal";
 
@@ -7,7 +7,44 @@ interface ITerminalControllerProps {}
 const TerminalController: React.FC<
   ITerminalControllerProps
 > = (): JSX.Element => {
-  return <Terminal />;
+  const [value, setValue] = useState<string>("");
+  const [history, setHistory] = useState<string[]>([]);
+  const bottomRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    // scroll to bottom every time history change
+    bottomRef?.current?.scrollIntoView({ behavior: "smooth" });
+  }, [history]);
+
+  const executeCommand = (command: string) => {
+    // do something for the command here!
+    // return a message to display
+
+    // for now just display the command in history
+    setHistory((prevState) => [...prevState, command]);
+  };
+
+  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(event.target.value);
+  };
+
+  const onKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === "Enter") {
+      event.preventDefault();
+      executeCommand(value);
+      setValue("");
+    }
+  };
+
+  return (
+    <Terminal
+      handleChange={onChange}
+      handleKeyDown={onKeyDown}
+      value={value}
+      history={history}
+      bottomRef={bottomRef}
+    />
+  );
 };
 
 export default TerminalController;
