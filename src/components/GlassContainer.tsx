61df7f5afa226b0fb994bd7863a78b56aae585c1
Frithjof Thorvik <frithjot@stud.ntnu.no>
2022-10-26 09:43:05 +0200

    add orders component

--- src/components/GlassContainer.tsx
+++ src/components/GlassContainer.tsx
@@ -3,17 +3,29 @@ import React from "react";
 import "./GlassContainer.scss";
 
 interface IGlassContainerProps {
+  border?: boolean;
+  shadow?: boolean;
   children: JSX.Element | JSX.Element[];
 }
 
 const GlassContainer: React.FC<IGlassContainerProps> = ({
   children,
+  border = false,
+  shadow = false,
 }): JSX.Element => {
+  const style = {
+    border: `${border ? "3px solid #94a3b8" : "none"}`,
+    borderRadius: `${border ? "15px" : "0px"}`,
+    boxShadow: `${shadow ? "0 5px 10px 1px #000000a0" : "none"}`,
+  };
+
   return (
-    <div className="glassContainer">
+    <div className="glassContainer" style={style}>
       <div className="content">{children}</div>
-      <div className="grain" />
-      <div className="triangle" />
+      <div className="layers">
+        <div className="grain" />
+        <div className="triangle" />
+      </div>
     </div>
   );
 };
